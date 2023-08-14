ef93a8df1d34e0e28016c7965de4501f321e0182
wangxiaobo <2078350663@qq.com>
2023-08-14 17:04:05 +0800

    fix sign in and sign up

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,5 @@
 class ApplicationController < ActionController::Base
-    # skip_before_action :verify_authenticity_token
-    # before_action :authenticate_user!
-    
+    before_action :authenticate_user!
 
     # def after_sign_in_path_for(_resource)
     #   posts_path
