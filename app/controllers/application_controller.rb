5f98092b594bf3e45550cf6c0e9c621ccf1181f1
wangxiaobo <2078350663@qq.com>
2023-08-16 09:11:18 +0800

    update

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,8 +5,9 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:username])
-    devise_parameter_sanitizer.permit(:sign_in, keys: [:login]) 
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:username, :email, :password, :password_confirmation])
+    devise_parameter_sanitizer.permit(:sign_in, keys: [:login, :password]) 
+    devise_parameter_sanitizer.permit(:account_update, keys: [:username, :name, :email, :password_confirmation, :current_password])
   end
 
     # def after_sign_in_path_for(_resource)
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,19 +1,17 @@
 class User < ApplicationRecord
+  attr_accessor :login
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-    attr_accessor :login
   
-    devise :database_authenticatable, :registerable,
-           :recoverable, :rememberable, :validatable
-  
-    def self.find_for_database_authentication(warden_conditions)
-      conditions = warden_conditions.dup
-      login = conditions.delete(:login)
-      where(conditions).where(["lower(username) = :value OR lower(email) = :value", { value: login.strip.downcase }]).first
-    end
+  def self.find_for_database_authentication(warden_condition)
+    conditions = warden_condition.dup
+    login = conditions.delete(:login)
+    where(conditions).where(
+      ["lower(username) = :value OR lower(email) = :value",
+      { value: login.strip.downcase}]).first
+  end
 
   has_many :posts, dependent: :destroy
 end
